9f60014aa35cf844858b40344be42fae05fc399f
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-05-19 11:27:24 +0530

    update

---
+++ lec 10/index.js
@@ -0,0 +1,4 @@
+localStorage.setItem("user","rahul")
+let data=localStorage.getItem("user")
+//localStorage.removeItem("user")
+console.log(data);
---
+++ lec 7/promise.js
@@ -0,0 +1,4 @@
+const arr={
+    Photo by <a href="https://unsplash.com/@planetvolumes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Planet Volumes</a> on <a href="https://unsplash.com/photos/motion-blurred-carousel-with-horses-at-night-qrbdpIpM5ig?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      
+}
