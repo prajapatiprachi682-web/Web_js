b9c2cd0e69f711b489796ec640726b47ac0fb49c
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-05-26 00:00:45 +0530

    update

---
+++ lec 14/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const App = () => {
+
+  let [count, SetCount] = useState("red")
+
+  function fun1() {
+    Setcolour("green")
+  }
+
+  return (
+    <div>style =
+    </div>
+  )
+}
+
+export default App
