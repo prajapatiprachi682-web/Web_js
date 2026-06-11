28fa2f4f93e78553d52bdd6399edfb662ad1dd53
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-11 21:52:47 +0530

    update

--- lec 14/my-react-app/src/main.jsx
+++ lec 14/my-react-app/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+import Context from '/Context.js'
 import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
